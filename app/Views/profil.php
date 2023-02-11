0eb0222b0b67078fa6efb97bb592219729989a97
bayu <litbang.bungamayang@gmail.com>
2023-02-11 10:01:17 +0700

    [!] Halaman profil

--- app/Views/profil.php
+++ app/Views/profil.php
@@ -13,10 +13,20 @@
       <!-- Page title -->
       <div class="page-header d-print-none">
         <div class="row align-items-center">
-          <div class="col">
+          <div class="row">
             <h2 class="page-title">
               Profil
             </h2>
+          </div>
+          <div class="row row-deck">
+            <div class="col-md-6">
+              <div class="card">
+                <div class="card-body">
+                  <p><h3><? echo session('nm_pegawai');?></h3></p>
+                  <p><? echo session('nm_jabatan')." ".ucwords(strtolower(session('nm_sub_divisi')));?></p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
